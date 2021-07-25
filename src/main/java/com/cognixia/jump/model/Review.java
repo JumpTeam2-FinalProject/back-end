819467bbd2d90cd8b217cae25daa8d473e1f10ee
Disto <48456332+DistortedAxiom@users.noreply.github.com>
2021-07-25 12:01:05 -0400

    model and repository

---
+++ src/main/java/com/cognixia/jump/model/Review.java
@@ -0,0 +1,103 @@
+package com.cognixia.jump.model;
+
+import java.util.Date;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+
+import com.cognixia.jump.model.User;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+public class Review {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer review_id;
+	
+	private Integer rating;
+	
+	private String review;
+	
+	private Date date;
+	
+	@ManyToOne
+	@JoinColumn(name = "restaurant_id", referencedColumnName = "restaurant_id")
+	@JsonBackReference
+	private Restaurant restaurant;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id", referencedColumnName = "user_id")
+	@JsonBackReference
+	private User user;
+	
+	
+
+	public Review(Integer review_id, Integer rating, String review, Date date, Restaurant restaurant,
+			User user) {
+		super();
+		this.review_id = review_id;
+		this.rating = rating;
+		this.review = review;
+		this.date = date;
+		this.restaurant = restaurant;
+		this.user = user;
+	}
+
+	public Integer getReview_id() {
+		return review_id;
+	}
+
+	public void setReview_id(Integer review_id) {
+		this.review_id = review_id;
+	}
+
+	public Integer getRating() {
+		return rating;
+	}
+
+	public void setRating(Integer rating) {
+		this.rating = rating;
+	}
+
+	public String getReview() {
+		return review;
+	}
+
+	public void setReview(String review) {
+		this.review = review;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public Restaurant getRestaurant() {
+		return restaurant;
+	}
+
+	public void setRestaurant(Restaurant restaurant) {
+		this.restaurant = restaurant;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	@Override
+	public String toString() {
+		return "Review [review_id=" + review_id + ", rating=" + rating + ", review=" + review + ", date=" + date + "]";
+	}
+	
+}
