35169428f68ecf3ef8c708ec360f781fbc567688
David <djknit@gmail.com>
2021-08-02 12:44:05 -0500

    fixed review post to work with front end

--- src/main/java/com/cognixia/jump/service/ReviewService.java
+++ src/main/java/com/cognixia/jump/service/ReviewService.java
@@ -16,7 +16,9 @@ import org.springframework.stereotype.Service;
 import com.cognixia.jump.model.Restaurant;
 import com.cognixia.jump.model.Review;
 import com.cognixia.jump.model.User;
+import com.cognixia.jump.repository.RestaurantRepository;
 import com.cognixia.jump.repository.ReviewRepository;
+import com.cognixia.jump.repository.UserRepository;
 import com.cognixia.jump.responsemodels.ReviewDetails;
 import com.cognixia.jump.util.DataFormatters;
 
@@ -25,6 +27,12 @@ public class ReviewService {
 	
 	@Autowired
 	ReviewRepository repo;
+	
+	@Autowired
+	UserRepository userRepo;
+	
+	@Autowired
+	RestaurantRepository restaurantRepo;
 	
 	public List<Review> getReviewsSimple() {
 		
@@ -49,7 +57,7 @@ public class ReviewService {
 		return new ReviewDetails(reviewOpt.get());
 	}
 	
-    public ReviewDetails addReview(Review newReview) {
+    public ReviewDetails addReview(Review newReview, Integer restaurant_id) {
 /***********
  * NOTE:
  * 		I (David) rewrote some of this method to reduce amount of code and make it work with my updates elsewhere.
@@ -63,7 +71,13 @@ public class ReviewService {
 //    	userHolder.setUserId(newReview.getUser_id());
 //    	review.setRestaurant(restaurantHolder);
 //    	review.setUser(userHolder);
-    	
+    	System.out.println("NEW REVIEW\n\n============================");
+    	System.out.println(newReview);
+    	System.out.println(newReview.getRestaurant());
+
+    	System.out.println(newReview.getUser());
+    	newReview.setUser(userRepo.getById(MyUserDetailsService.getCurrentUserId()));
+    	newReview.setRestaurant(restaurantRepo.getById(restaurant_id));
     	LocalDate localDate = LocalDate.now();
     	
     	Date date = java.sql.Date.valueOf(localDate);
